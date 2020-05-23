b1009226a38e963f3ab9a3b59c2b460e17f468ed
chris chen <y_zhengchen@163.com>
2020-05-23 13:48:25 -0400

    add global exception handler for mvc

--- src/main/java/com/wequan/bu/controller/TutorController.java
+++ src/main/java/com/wequan/bu/controller/TutorController.java
@@ -1,11 +1,14 @@
 package com.wequan.bu.controller;
 
+import com.wequan.bu.exception.NotImplementedException;
 import com.wequan.bu.repository.model.Tutor;
 import com.wequan.bu.service.TutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.List;
 
@@ -24,5 +27,15 @@ public class TutorController {
         System.out.println(tutorService.findAll().stream().count());
         List<Tutor> allTutors = tutorService.findAll();
         return allTutors;
+    }
+
+    @GetMapping("/info")
+    public List<Tutor> getInfo(@RequestParam("id") String id) throws NotImplementedException {
+        throw new NotImplementedException();
+    }
+
+    @GetMapping("/appointments")
+    public List<Tutor> getAppointments(@RequestParam("id") String id) throws NoHandlerFoundException {
+        throw new NoHandlerFoundException("get","example.com",null);
     }
 }
