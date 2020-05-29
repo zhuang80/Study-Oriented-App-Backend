9522899ef7844d7b4ef878011ad3b44de796f4b6
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-05-29 18:45:56 -0400

    added CourseMapper interface

---
+++ src/main/java/com/wequan/bu/repository/dao/CourseMapper.java
@@ -0,0 +1,12 @@
+package com.wequan.bu.repository.dao;
+
+import com.wequan.bu.repository.model.Course;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Zhaochao Huang
+ */
+@Mapper
+public interface CourseMapper extends GeneralMapper<Course> {
+
+}
