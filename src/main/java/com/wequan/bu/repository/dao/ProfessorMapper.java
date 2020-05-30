5e6262ebeeca185e467dc619d330ff30be30352d
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-05-29 22:52:42 -0400

    added ProfessorMapper interface

---
+++ src/main/java/com/wequan/bu/repository/dao/ProfessorMapper.java
@@ -0,0 +1,12 @@
+package com.wequan.bu.repository.dao;
+
+import com.wequan.bu.repository.model.Professor;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Zhaochao Huang
+ */
+@Mapper
+public interface ProfessorMapper extends GeneralMapper<Professor> {
+
+}
