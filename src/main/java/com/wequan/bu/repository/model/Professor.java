788fa816c0190a0ace5d44d8f2066a3f23d9933c
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-05-29 22:50:15 -0400

    added Professor model class

---
+++ src/main/java/com/wequan/bu/repository/model/Professor.java
@@ -0,0 +1,84 @@
+package com.wequan.bu.repository.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Zhaochao Huang
+ */
+public class Professor {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private Integer schoolId;
+    private Integer overallScore;
+    private LocalDate createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Integer getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(Integer schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public Integer getOverallScore() {
+        return overallScore;
+    }
+
+    public void setOverallScore(Integer overallScore) {
+        this.overallScore = overallScore;
+    }
+
+    public LocalDate getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDate createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Integer createBy) {
+        this.createBy = createBy;
+    }
+
+    private Integer createBy;
+
+
+}
