562056f186ee89876e052fae32de8fce327b296e
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-05-30 17:56:58 -0400

    added ProfessorCourseRate model class

--- src/main/java/com/wequan/bu/controller/ProfessorController.java
+++ src/main/java/com/wequan/bu/controller/ProfessorController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,4 +26,8 @@ public class ProfessorController {
     public List<Professor> findAll(){
         return professorService.findAll();
     }
+
+    @GetMapping("/professor/{id}")
+    @ApiOperation(value="findById", notes="find a specific professor by its id")
+    public Professor findById(@PathVariable("id") Integer id) { return professorService.findById(id); }
 }
