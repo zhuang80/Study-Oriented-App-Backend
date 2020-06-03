5d9010c8f9ad63a68fddec72fa890f8abd49522e
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-02 22:08:43 -0400

    added setter and getter

--- src/main/java/com/wequan/bu/controller/vo/MaterialForm.java
+++ src/main/java/com/wequan/bu/controller/vo/MaterialForm.java
@@ -11,4 +11,44 @@ public class MaterialForm {
     private Integer studyPointsRequired;
     private String term;
     private MultipartFile[] files;
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getSourceFrom() {
+        return sourceFrom;
+    }
+
+    public void setSourceFrom(String sourceFrom) {
+        this.sourceFrom = sourceFrom;
+    }
+
+    public Integer getStudyPointsRequired() {
+        return studyPointsRequired;
+    }
+
+    public void setStudyPointsRequired(Integer studyPointsRequired) {
+        this.studyPointsRequired = studyPointsRequired;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public MultipartFile[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(MultipartFile[] files) {
+        this.files = files;
+    }
 }
