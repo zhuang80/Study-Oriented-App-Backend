90b686f9751e21cf4b2c662e13754262b4b993cc
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-04 18:46:23 -0400

    able to convert the pdf files sent from frontend to images

--- src/main/java/com/wequan/bu/controller/MaterialController.java
+++ src/main/java/com/wequan/bu/controller/MaterialController.java
@@ -3,6 +3,9 @@ package com.wequan.bu.controller;
 import com.wequan.bu.controller.vo.MaterialForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
@@ -30,14 +35,28 @@ public class MaterialController {
         for(MultipartFile file : files) {
             if(file.getSize() > 0){
                 //assign a new unique name for the uploaded file
+                System.out.println(file.getContentType());
                 String filename = file.getOriginalFilename();
                 String[] temps = filename.split("\\.", 2);
                 UUID randomUUID = UUID.randomUUID();
                 String newFilename = temps[0] + "-" + randomUUID + "." + temps[1];
                 //hardcode the file path
-                String path = "C:\\Users\\huang\\Desktop\\Wequan\\" + newFilename;
+                String path = "C:\\Users\\huang\\Desktop\\Wequan\\LocalStorage\\" + newFilename;
                 File newFile = new File(path);
                 file.transferTo(newFile);
+                if(file.getContentType().equals("application/pdf")){
+                    PDDocument pd = PDDocument.load(newFile);
+                    PDFRenderer pr = new PDFRenderer(pd);
+                    for(int page = 0; page < pd.getNumberOfPages(); ++page){
+                        if(page > 2) break;
+                        BufferedImage bi = pr.renderImageWithDPI(page, 72, ImageType.RGB);
+                        String newImage = "C:\\Users\\huang\\Desktop\\Wequan\\LocalStorage\\"+
+                                temps[0] + "-" + randomUUID + "-page-" + (page+1)+".jpg";
+                        ImageIO.write(bi, "JPEG", new File(newImage));
+                    }
+
+
+                }
             }
         }
         return "success";
