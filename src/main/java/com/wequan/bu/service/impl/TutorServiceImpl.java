ded5448b2493e777e0f49aaf2fcc9ca1c222c075
zhen <greatcarnegie@gmail.com>
2020-06-05 06:51:12 -0400

    Add available tutor APIs

--- src/main/java/com/wequan/bu/service/impl/TutorServiceImpl.java
+++ src/main/java/com/wequan/bu/service/impl/TutorServiceImpl.java
@@ -4,12 +4,11 @@ import com.wequan.bu.repository.dao.TutorMapper;
 import com.wequan.bu.repository.model.Tutor;
 import com.wequan.bu.service.AbstractService;
 import com.wequan.bu.service.TutorService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * @author ChrisChen
@@ -17,14 +16,17 @@ import javax.annotation.PostConstruct;
 @Service
 public class TutorServiceImpl extends AbstractService<Tutor> implements TutorService {
 
-    private static final Logger logger = LoggerFactory.getLogger(TutorServiceImpl.class);
-
     @Autowired
     private TutorMapper tutorMapper;
 
     @PostConstruct
     public void postConstruct() {
         this.setMapper(tutorMapper);
+    }
+
+    @Override
+    public List<Tutor> findTutors(Integer subjectId) {
+        return tutorMapper.selectTutors(subjectId);
     }
 
 }
