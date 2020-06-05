9196412d2b68177921dd048d02de65a02ad21736
chris chen <y_zhengchen@163.com>
2020-06-04 20:52:42 -0400

    example code for tutor search

--- src/main/java/com/wequan/bu/service/SearchService.java
+++
@@ -1,9 +0,0 @@
-package com.wequan.bu.service;
-
-/**
- * @author ChrisChen
- */
-public interface SearchService {
-
-
-}
--- src/main/java/com/wequan/bu/service/TutorService.java
+++ src/main/java/com/wequan/bu/service/TutorService.java
@@ -2,8 +2,21 @@ package com.wequan.bu.service;
 
 import com.wequan.bu.repository.model.Tutor;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author ChrisChen
  */
 public interface TutorService extends Service<Tutor> {
+
+    /**
+     * 按搜索条件搜索tutor
+     * @param whereCondition where
+     * @param groupCondition group by
+     * @param orderCondition order by
+     * @param pageCondition page
+     * @return Tutor列表
+     */
+    List<Tutor> search(String whereCondition, String groupCondition, String orderCondition, Map<String, Integer> pageCondition);
 }
