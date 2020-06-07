8e7b9bbfe346a4d5c1b8d2f2422f880e63431add
chris chen <y_zhengchen@163.com>
2020-06-07 10:58:16 -0400

    update result and global exception

--- src/main/java/com/wequan/bu/config/WeQuanConfiguration.java
+++ src/main/java/com/wequan/bu/config/WeQuanConfiguration.java
@@ -1,8 +1,14 @@
 package com.wequan.bu.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wequan.bu.config.handler.MessageHandler;
 import com.wequan.bu.config.properties.SwaggerProperties;
+import com.wequan.bu.controller.vo.result.Result;
+import com.wequan.bu.controller.vo.result.ResultCode;
+import com.wequan.bu.exception.ServiceException;
 import org.apache.catalina.connector.Connector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -12,6 +18,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -21,13 +31,19 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author ChrisChen
  */
 @Configuration
 public class WeQuanConfiguration implements WebMvcConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(WeQuanConfiguration.class);
 
     @Value("${http.port}")
     private Integer port;
@@ -87,6 +103,49 @@ public class WeQuanConfiguration implements WebMvcConfigurer {
         factory.setMaxFileSize(DataSize.ofMegabytes(10));
         factory.setMaxRequestSize(DataSize.ofMegabytes(10));
         return factory.createMultipartConfig();
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add((request, response, handler, ex) -> {
+            Result result = new Result();
+            if (ex instanceof ServiceException) {
+                //业务失败的异常，如"账号或密码错误"
+                result.setCode(ResultCode.FAIL).setMessage(ex.getMessage());
+                log.info(ex.getMessage());
+            } else if (ex instanceof NoHandlerFoundException) {
+                result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
+            } else if (ex instanceof ServletException) {
+                result.setCode(ResultCode.FAIL).setMessage(ex.getMessage());
+            } else {
+                result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误");
+                String message;
+                if (handler instanceof HandlerMethod) {
+                    HandlerMethod handlerMethod = (HandlerMethod) handler;
+                    message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
+                            request.getRequestURI(),
+                            handlerMethod.getBean().getClass().getName(),
+                            handlerMethod.getMethod().getName(),
+                            ex.getMessage());
+                } else {
+                    message = ex.getMessage();
+                }
+                log.error(message, ex);
+            }
+            responseResult(response, result);
+            return new ModelAndView();
+        });
+    }
+
+    private void responseResult(HttpServletResponse response, Result result) {
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-type", "application/json;charset=UTF-8");
+        response.setStatus(200);
+        try {
+            response.getWriter().write(new ObjectMapper().writeValueAsString(result));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 
     private String getIpAddress(HttpServletRequest request) {
