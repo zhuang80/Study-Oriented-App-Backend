56a772a633d5c55dbbdb97e1721c99f359e3d255
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-10 12:55:17 -0400

    changed base path to /study_space for SchoolController, exposed a new endpoint GET /study_space/departments?school_id

--- src/main/java/com/wequan/bu/controller/SchoolController.java
+++ src/main/java/com/wequan/bu/controller/SchoolController.java
@@ -1,12 +1,17 @@
 package com.wequan.bu.controller;
 
+import com.wequan.bu.config.handler.MessageHandler;
+import com.wequan.bu.repository.model.Department;
 import com.wequan.bu.repository.model.School;
 import com.wequan.bu.service.SchoolService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,15 +20,30 @@ import java.util.List;
  * @author Zhaochao Huang
  */
 @RestController
-@RequestMapping("/universities")
+@RequestMapping("/study_space")
 @Api(value = "对学校的操作", tags = "学校相关Rest Api")
 public class SchoolController {
+
+    private static final Logger log = LoggerFactory.getLogger(SchoolController.class);
+
+    @Autowired
+    private MessageHandler messageHandler;
     @Autowired
     private SchoolService schoolService;
 
-    @GetMapping("")
+    @GetMapping("/universities")
     @ApiOperation(value = "findAll", notes = "return a list of schools")
     public List<School> findAll(Integer pageNum, Integer pageSize){
         return schoolService.findAll(pageNum, pageSize);
+    }
+
+    @GetMapping("/departments")
+    @ApiOperation(value = "", notes = "return a list of departments by school id")
+    public List<Department> findDepartmentsBySchoolId(@RequestParam("schoolId") Integer id){
+        if(id < 0){
+            log.info(messageHandler.getFailResponseMessage("40008"));
+            return null;
+        }
+        return schoolService.findDepartmentsBySchoolId(id);
     }
 }
