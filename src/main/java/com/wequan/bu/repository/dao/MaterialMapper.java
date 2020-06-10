747b8efa3b98df2a78fe95feecad187cfd772e90
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-10 15:01:21 -0400

    added MaterialMapper interface

---
+++ src/main/java/com/wequan/bu/repository/dao/MaterialMapper.java
@@ -0,0 +1,11 @@
+package com.wequan.bu.repository.dao;
+
+import com.wequan.bu.repository.model.Material;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Zhaochao Huang
+ */
+@Mapper
+public interface MaterialMapper extends GeneralMapper<Material>{
+}
