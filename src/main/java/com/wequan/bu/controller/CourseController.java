2e7f8bac0c94932f94ad5c0d1bbb271b690e13f5
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-12 18:46:04 -0400

    fix: fix merge conflict and change the return type to Result in CourseController

--- src/main/java/com/wequan/bu/controller/CourseController.java
+++ src/main/java/com/wequan/bu/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.wequan.bu.controller;
 import com.wequan.bu.config.handler.MessageHandler;
 import com.wequan.bu.controller.vo.CourseVo;
 import com.wequan.bu.controller.vo.result.Result;
+import com.wequan.bu.controller.vo.result.ResultCode;
 import com.wequan.bu.controller.vo.result.ResultGenerator;
 import com.wequan.bu.json.JSON;
 import com.wequan.bu.repository.model.Course;
@@ -37,23 +38,32 @@ public class CourseController {
 
     @PostMapping("/course")
     @ApiOperation(value = "add course", notes = "添加课程")
-    public Result addCourse(@RequestBody Course course) {
-        Result result = ResultGenerator.success();
+    public Result addCourse(@RequestBody CourseVo course) {
+        Result result;
+        try{
+            courseService.save(course);
+        }catch(Exception e){
+            String message = messageHandler.getFailResponseMessage(e.getMessage());
+            result = ResultGenerator.fail(message);
+            return result;
+        }
+        result = ResultGenerator.success();
         return result;
     }
 
-    @GetMapping("/courses")
+    @GetMapping("/search/courses")
     @ApiOperation(value="Search course", notes="根据课程名或者课程代号返回课程列表，关联授课professor的 name, rating")
     @JSON(type = Professor.class, filter = {"courses", "courseRates", "department", "school"})
-    public List<Course> findCourses(@RequestParam(value = "name", required = false) String name,
+    public Result<List<Course>> findCourses(@RequestParam(value = "name", required = false) String name,
                                     @RequestParam(value = "code", required = false) String code) {
         List<Course> courseList = null;
-        if (StringUtils.hasText(name) && StringUtils.hasText(code)) {
+        if (StringUtils.hasText(name) || StringUtils.hasText(code)) {
             courseList = courseService.findByNameOrCode(name, code);
         } else {
             courseList = courseService.findAll();
         }
-        return courseList;
+        Result result = ResultGenerator.success(courseList);
+        return result;
     }
 
     @GetMapping("/course/top")
@@ -69,47 +79,31 @@ public class CourseController {
     @GetMapping("/course/{id}")
     @ApiOperation(value="Course profile", notes="获取course信息，关联授课professor信息以及可用课程资料类型")
     @JSON(type = Professor.class, filter = {"courses", "courseRates", "department", "school"})
-    public Course findById(@PathVariable("id") Integer id) {
+    public Result<Course> findById(@PathVariable("id") Integer id) {
+        Result result;
         if(id<0){
-            log.info(messageHandler.getFailResponseMessage("40003"));
-            return null;
+            String message = messageHandler.getFailResponseMessage("40003");
+            result = ResultGenerator.fail(message);
+            return result;
         }
-        return courseService.findByIdAssociatedWithProfessor(id);
+        Course course = courseService.findByIdAssociatedWithProfessor(id);
+        result = ResultGenerator.success(course);
+        return result;
     }
 
     @GetMapping("/course/{id}/professors")
     @ApiOperation(value="a list of professors who teach required course", notes="根据course id获取授课教师列表")
     @ResponseBody
-    public List<Professor> findProfessorsByCourseId(@PathVariable("id") Integer id){
+    public Result<List<Professor>> findProfessorsByCourseId(@PathVariable("id") Integer id){
+        Result result;
         if(id < 0) {
-            log.info(messageHandler.getFailResponseMessage("40003"));
-            return null;
+            String message = messageHandler.getFailResponseMessage("40003");
+            result = ResultGenerator.fail(ResultCode.FAIL.code(), message);
+            return result;
         }
-        return professorService.findProfessorsByCourseId(id);
+        List<Professor> professorList = professorService.findProfessorsByCourseId(id);
+        result = ResultGenerator.success(professorList);
+        return result;
     }
 
-<<<<<<< HEAD
-    @GetMapping("/search/course")
-    @ApiOperation(value = "", notes= "return a list of course with associated information by name or code")
-    @JSON(type = Professor.class, filter = {"courses", "courseRates", "department", "school"})
-    public List<Course> findByNameOrCode(@RequestParam("name") String name, @RequestParam("code") String code){
-        if(name.isEmpty() && code.isEmpty()) {
-            log.info(messageHandler.getFailResponseMessage("40004"));
-            return null;
-        }
-        return courseService.findByNameOrCode(name, code);
-    }
-
-    @PostMapping("/course")
-    @ApiOperation(value="", notes="add a course")
-    @ResponseBody
-    public void addCourse(@RequestBody CourseVo course){
-        try{
-            courseService.save(course);
-        }catch(Exception e){
-            log.info(messageHandler.getFailResponseMessage(e.getMessage()));
-        }
-    }
-=======
->>>>>>> master
 }
