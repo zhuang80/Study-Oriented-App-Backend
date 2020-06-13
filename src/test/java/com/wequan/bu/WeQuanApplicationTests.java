138c3e976674e5f61df47ccc6d02730b7e3a5b6d
chris chen <y_zhengchen@163.com>
2020-06-13 13:45:26 -0400

    add event and test case example for future use

---
+++ src/test/java/com/wequan/bu/WeQuanApplicationTests.java
@@ -0,0 +1,26 @@
+package com.wequan.bu;
+
+import com.wequan.bu.controller.vo.StudyPointHistory;
+import com.wequan.bu.event.StudyPointEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+class WeQuanApplicationTests extends BaseTester {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void eventTest() {
+		StudyPointHistory pointHistory = new StudyPointHistory();
+		pointHistory.setAmount(-100);
+		StudyPointEvent studyPointEvent = new StudyPointEvent(pointHistory);
+		applicationContext.publishEvent(studyPointEvent);
+	}
+
+}
