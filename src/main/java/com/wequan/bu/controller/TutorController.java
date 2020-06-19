4c6befd5e6169c4166a639b85991f43afdf44254
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-19 15:43:55 -0400

    remove: sun.plugin2.message

--- src/main/java/com/wequan/bu/controller/TutorController.java
+++ src/main/java/com/wequan/bu/controller/TutorController.java
@@ -18,7 +18,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin2.message.Message;
 
 import java.util.List;
 import java.util.Map;
