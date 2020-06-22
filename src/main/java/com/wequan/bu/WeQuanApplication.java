e67b373a80f808ac7bba97a5f820024c56ea52d5
zhen <greatcarnegie@gmail.com>
2020-06-22 18:23:41 -0400

    update WeQuanApplication

--- src/main/java/com/wequan/bu/WeQuanApplication.java
+++ src/main/java/com/wequan/bu/WeQuanApplication.java
@@ -3,6 +3,7 @@ package com.wequan.bu;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class WeQuanApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WeQuanApplication.class, args);
 	}
 
