76d9957da8a98a4f1cf57302ab9c508b36f031d9
zhen <greatcarnegie@gmail.com>
2020-06-23 17:12:55 -0400

    add leaf-segment service

---
+++ src/main/java/com/wequan/bu/im/idgen/segment/dao/IDAllocDao.java
@@ -0,0 +1,12 @@
+package com.wequan.bu.im.idgen.segment.dao;
+
+import com.wequan.bu.im.idgen.segment.model.LeafAlloc;
+
+import java.util.List;
+
+public interface IDAllocDao {
+     List<LeafAlloc> getAllLeafAllocs();
+     LeafAlloc updateMaxIdAndGetLeafAlloc(String tag);
+     LeafAlloc updateMaxIdByCustomStepAndGetLeafAlloc(LeafAlloc leafAlloc);
+     List<String> getAllTags();
+}
