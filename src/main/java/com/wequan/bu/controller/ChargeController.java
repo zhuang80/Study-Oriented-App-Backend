ef982ac53cb57886b0214c88eab48bda9777627e
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-27 17:09:59 -0400

    add: add a webhook to fulfill purchase

--- src/main/java/com/wequan/bu/controller/ChargeController.java
+++ src/main/java/com/wequan/bu/controller/ChargeController.java
@@ -1,13 +1,17 @@
 package com.wequan.bu.controller;
 
+import com.nimbusds.oauth2.sdk.auth.ClientSecretPost;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.WebhookEndpoint;
 import com.wequan.bu.controller.vo.result.Result;
 import com.wequan.bu.controller.vo.result.ResultGenerator;
 import com.wequan.bu.service.StripeService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Zhaochao Huang
@@ -23,6 +27,19 @@ public class ChargeController {
     public Result connect(@RequestParam("code") String code,
                           @RequestParam("tutorId") Integer tutorId){
         stripeService.storeConnectedId(code, tutorId);
+        return ResultGenerator.success();
+    }
+
+    @GetMapping("/client_secret")
+    public Result<String> getClientSecret(@RequestParam("tutorId") Integer tutorId) throws StripeException {
+        PaymentIntent paymentIntent = stripeService.createPaymentIntent(tutorId);
+        return ResultGenerator.success(paymentIntent.getClientSecret());
+    }
+
+    @PostMapping("/webhook")
+    public Result fulfillPurchase(HttpServletRequest request,
+                                  @RequestBody String webhookEndpoint){
+        stripeService.fulfillPurchase(request.getHeader("Stripe-Signature"), webhookEndpoint);
         return ResultGenerator.success();
     }
 }
--- src/main/java/com/wequan/bu/repository/dao/TutorStripeMapper.java
+++ src/main/java/com/wequan/bu/repository/dao/TutorStripeMapper.java
@@ -2,10 +2,12 @@ package com.wequan.bu.repository.dao;
 
 import com.wequan.bu.repository.model.TutorStripe;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @author Zhaochao Huang
  */
 @Mapper
 public interface TutorStripeMapper extends GeneralMapper<TutorStripe>{
+    public TutorStripe selectByTutorId(@Param("tutor_id") Integer tutorId);
 }
