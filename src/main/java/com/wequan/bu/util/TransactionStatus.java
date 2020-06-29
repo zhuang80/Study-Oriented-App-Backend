7ec0ac69b6e3a09028e00a7c6ae3c79b6e02beff
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-06-28 20:45:56 -0400

    fix: have webhook to handle Transaction storage when a payment_intent.created event occuers

---
+++ src/main/java/com/wequan/bu/util/TransactionStatus.java
@@ -0,0 +1,12 @@
+package com.wequan.bu.util;
+
+public enum TransactionStatus {
+    REQUIRES_PAYMENT_METHOD((short) 0), PROCESSING((short)1), CANCELED((short)2), SUCCEEDED((short)3);
+    private final short value;
+    private TransactionStatus(short value){
+        this.value = value;
+    }
+    public short getValue(){
+        return value;
+    }
+}
