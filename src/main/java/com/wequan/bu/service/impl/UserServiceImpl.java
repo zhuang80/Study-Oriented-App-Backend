d3d0f7c3229a1348c98361f9ee44abef1bdbaf9b
chris chen <y_zhengchen@163.com>
2020-06-30 09:39:38 -0400

    get /user/{id}/followers, /user/{id}/following; post /user/{id}/follow

--- src/main/java/com/wequan/bu/service/impl/UserServiceImpl.java
+++ src/main/java/com/wequan/bu/service/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.wequan.bu.service.impl;
 
+import com.wequan.bu.repository.dao.UserFollowMapper;
 import com.wequan.bu.repository.dao.UserMapper;
 import com.wequan.bu.repository.model.User;
+import com.wequan.bu.repository.model.UserFollow;
+import com.wequan.bu.repository.model.extend.UserFollowBriefInfo;
 import com.wequan.bu.repository.model.extend.UserStats;
 import com.wequan.bu.service.AbstractService;
 import com.wequan.bu.service.UserService;
 import com.wequan.bu.vendor.AwsEmailService;
+import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author ChrisChen
@@ -24,6 +32,8 @@ public class UserServiceImpl extends AbstractService<User> implements UserServic
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private UserFollowMapper userFollowMapper;
     @Autowired
     private AwsEmailService emailService;
 
@@ -66,6 +76,35 @@ public class UserServiceImpl extends AbstractService<User> implements UserServic
     @Override
     public UserStats getUserProfile(Integer userId) {
         return userMapper.selectById(userId);
+    }
+
+    @Override
+    public void followOtherUser(Integer userId, Integer otherUserId, Integer action) {
+        if (action == 1) {
+            UserFollow userFollow = new UserFollow();
+            userFollow.setUserId(userId);
+            userFollow.setFollowId(otherUserId);
+            userFollow.setCreateTime(new Date());
+            userFollowMapper.insertSelective(userFollow);
+        }
+        if (action == -1) {
+            Map<String, Integer> params = new HashMap<>(2);
+            params.put("userId", userId);
+            params.put("followId", otherUserId);
+            userFollowMapper.deleteByPrimaryKey(params);
+        }
+    }
+
+    @Override
+    public List<UserFollowBriefInfo> getUserFollowing(Integer userId, Integer pageNum, Integer pageSize) {
+        RowBounds rowBounds = new RowBounds(pageNum, pageSize);
+        return userFollowMapper.selectFollowingByUserId(userId, rowBounds);
+    }
+
+    @Override
+    public List<UserFollowBriefInfo> getUserFollower(Integer userId, Integer pageNum, Integer pageSize) {
+        RowBounds rowBounds = new RowBounds(pageNum, pageSize);
+        return userFollowMapper.selectFollowerByUserId(userId, rowBounds);
     }
 
 
