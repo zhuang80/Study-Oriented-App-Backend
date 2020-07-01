7e5be9965d70a2f394df13c3756ed4984ca0fe34
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-07-01 12:42:01 -0400

    add: admin is able to view pending refund applications, then approve or reject applications

--- src/main/java/com/wequan/bu/repository/dao/AppointmentChangeRecordMapper.java
+++ src/main/java/com/wequan/bu/repository/dao/AppointmentChangeRecordMapper.java
@@ -3,9 +3,13 @@ package com.wequan.bu.repository.dao;
 import com.wequan.bu.repository.model.AppointmentChangeRecord;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author Zhaochao Huang
  */
 @Mapper
 public interface AppointmentChangeRecordMapper extends GeneralMapper<AppointmentChangeRecord> {
+
+    public List<AppointmentChangeRecord> selectPendingRefundApplication();
 }
--- src/main/java/com/wequan/bu/service/AppointmentChangeRecordService.java
+++ src/main/java/com/wequan/bu/service/AppointmentChangeRecordService.java
@@ -2,9 +2,17 @@ package com.wequan.bu.service;
 
 import com.wequan.bu.repository.model.AppointmentChangeRecord;
 
+import java.util.List;
+
 public interface AppointmentChangeRecordService extends Service<AppointmentChangeRecord> {
 
     public void addRecordByTutor(String transactionId, Integer tutorId);
 
     public void addRecordByUser(String transactionId, Integer userId, Integer refundAmount);
+
+    public List<AppointmentChangeRecord> findPendingRefundApplication();
+
+    public void approve(Integer id, String comment);
+
+    public void reject(Integer id, String comment);
 }
