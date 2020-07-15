3aef70d9da9151b7786ef0ab4bce12cf3dc0cbcc
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-07-14 23:40:35 -0400

    add: complete Post /course/associate_professor endpoint

--- src/main/java/com/wequan/bu/service/impl/CourseServiceImpl.java
+++ src/main/java/com/wequan/bu/service/impl/CourseServiceImpl.java
@@ -4,14 +4,18 @@ import com.github.pagehelper.PageHelper;
 import com.wequan.bu.controller.vo.CourseVo;
 import com.wequan.bu.repository.dao.CategoryMapper;
 import com.wequan.bu.repository.dao.CourseMapper;
+import com.wequan.bu.repository.dao.ProfessorCourseMapper;
 import com.wequan.bu.repository.model.Category;
 import com.wequan.bu.repository.model.Course;
+import com.wequan.bu.repository.model.ProfessorCourse;
 import com.wequan.bu.service.AbstractService;
 import com.wequan.bu.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -21,6 +25,10 @@ import java.util.List;
 public class CourseServiceImpl extends AbstractService<Course> implements CourseService {
     @Autowired
     private CourseMapper courseMapper;
+
+    @Autowired
+    private ProfessorCourseMapper professorCourseMapper;
+
     @Autowired
     private CategoryMapper categoryMapper;
 
@@ -49,6 +57,17 @@ public class CourseServiceImpl extends AbstractService<Course> implements Course
         PageHelper.startPage(pageNum, pageSize);
 
         return courseMapper.selectTopViewedCourses(schoolId, subjectId);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void associateWithProfessor(Integer courseId, Integer professorId, Integer userId) {
+        ProfessorCourse professorCourse = new ProfessorCourse();
+        professorCourse.setProfessorId(professorId);
+        professorCourse.setCourseId(courseId);
+        professorCourse.setCreateBy(userId);
+        professorCourse.setCreateTime(LocalDateTime.now());
+        professorCourseMapper.insertSelective(professorCourse);
     }
 
 }
