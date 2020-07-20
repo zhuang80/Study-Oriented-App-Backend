069b02cc2fa89044a6453d2dda90470e5570d761
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-07-19 22:35:11 -0400

    fix: change webhook secret

--- src/main/java/com/wequan/bu/service/impl/StripeServiceImpl.java
+++ src/main/java/com/wequan/bu/service/impl/StripeServiceImpl.java
@@ -52,10 +52,8 @@ public class StripeServiceImpl extends AbstractService<TutorStripe> implements S
     @Value("${CLIENT_ID}")
     private String clientId;
 
-    /*
     @Value("${PAYMENT_INTENT_WEBHOOK_SECRET}")
     private String paymentIntentWebhookSecret;
-
 
     @Value("${REFUND_WEBHOOK_SECRET}")
     private String refundWebhookSecret;
@@ -65,13 +63,14 @@ public class StripeServiceImpl extends AbstractService<TutorStripe> implements S
 
     @Value("${TRANSFER_WEBHOOK_SECRET}")
     private String transferWebhookSecret;
-*/
+
  //local test webhook secret
+/*
     private String paymentIntentWebhookSecret = "whsec_UYCgjzmqTIMbBgZsuI3mxc63mD9YaHdi";
     private String refundWebhookSecret="whsec_UYCgjzmqTIMbBgZsuI3mxc63mD9YaHdi";
     private String accountWebhookSecret = "whsec_UYCgjzmqTIMbBgZsuI3mxc63mD9YaHdi";
     private String transferWebhookSecret = "whsec_UYCgjzmqTIMbBgZsuI3mxc63mD9YaHdi";
-
+*/
 
     @Autowired
     private TutorStripeMapper tutorStripeMapper;
