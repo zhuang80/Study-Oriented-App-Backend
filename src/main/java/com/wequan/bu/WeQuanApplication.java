cf47bb7ebe1cec28a2bc45d289ef540c7fade0fe
chris chen <y_zhengchen@163.com>
2020-07-23 19:48:03 -0400

    fix bug when favorite again; create study point rule; add event for view history

--- src/main/java/com/wequan/bu/WeQuanApplication.java
+++ src/main/java/com/wequan/bu/WeQuanApplication.java
@@ -3,6 +3,7 @@ package com.wequan.bu;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableTransactionManagement
 @EnableScheduling
+@EnableAsync
 public class WeQuanApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
