cf891ee4d28262c91297136651ae800a340359b6
zhuang80 <35575522+zhuang80@users.noreply.github.com>
2020-07-31 16:04:28 -0400

    add: add get /cancellation_policy and get /late_policy api

---
+++ src/main/java/com/wequan/bu/repository/dao/LatePolicyMapper.java
@@ -0,0 +1,11 @@
+package com.wequan.bu.repository.dao;
+
+import com.wequan.bu.repository.model.LatePolicy;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Zhaochao Huang
+ */
+@Mapper
+public interface LatePolicyMapper extends GeneralMapper<LatePolicy>{
+}
